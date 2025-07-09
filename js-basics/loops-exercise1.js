29562ae5e9d811e9e1a986350d2bdc75cedd5020
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-07-09 14:14:05 +0000

    Add JS looping exercise

---
+++ js-basics/loops-exercise1.js
@@ -0,0 +1,9 @@
+console.log("Let's count from 10 in reverse.");
+
+//let count = 10;
+
+for (let count = 10; count > 0 ; count--) {
+	console.log(`${count}...`)
+};
+
+console.log("Hurray! (just so you know, I suffered to do this).")
