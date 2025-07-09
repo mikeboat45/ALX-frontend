0d2f5867bed1e782dda89cccd446aa61b5ac5738
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-07-09 12:59:57 +0000

    Add JS variables exercise files

---
+++ js-basics/JS-variables-exercise1.js
@@ -0,0 +1,25 @@
+console.log("Working with Variables and Data Types");
+
+let name = "Michael";
+console.log(name, typeof(name));
+
+let age = 24;
+console.log(age, typeof(age));
+
+let isStudent = true;
+console.log(isStudent, typeof(isStudent));
+
+const address = "Amasaman, Stadium Road";
+console.log(address, typeof(address));
+
+let hobbies = ["coding", "Bible Study", "football"];
+console.log(hobbies, typeof(hobbies));
+console.log(hobbies[1]);
+
+let undefinedVariable;
+console.log(undefinedVariable, typeof(undefineVariable));
+
+let nullVariable = null;
+console.log(nullVariable, typeof(nullVariable));
+
+
