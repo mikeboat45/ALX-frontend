5b6a9d44c3cf652992101ce461d661d2a8337320
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-07-09 13:27:39 +0000

    Add JS conditions exercise

---
+++ js-basics/conditions-exercise1.js
@@ -0,0 +1,11 @@
+let number = 4;
+
+if (number > 0) {
+	console.log(`${number} is positive`);
+} else if (number < 0) {
+	console.log(`${number} is negative`);
+} else {
+	console.log(`${number} is 0`)
+}
+
+console.log("I'm awesome!")
